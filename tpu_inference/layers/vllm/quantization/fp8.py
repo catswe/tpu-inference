0d8ca8d4964ec1ebcd8ae4ee167fc1baaf704c57
catswe <212922539+catswe@users.noreply.github.com>
2026-02-01 05:51:35 +0000

    Format
    Signed-off-by: catswe <212922539+catswe@users.noreply.github.com>

--- tpu_inference/layers/vllm/quantization/fp8.py
+++ tpu_inference/layers/vllm/quantization/fp8.py
@@ -37,9 +37,9 @@ from tpu_inference.layers.common.process_weights.linear_weights import (
     LinearWeights, process_linear_weights, shard_linear_weights,
     to_parameter_list)
 from tpu_inference.layers.common.quant_methods import FP8, get_tpu_quant_method
-from tpu_inference.layers.common.quantization import pad_and_dequantize_tensor
 from tpu_inference.layers.common.quantization import fp8 as common_fp8
-from tpu_inference.layers.common.quantization import quantize_tensor
+from tpu_inference.layers.common.quantization import (
+    pad_and_dequantize_tensor, quantize_tensor)
 from tpu_inference.layers.common.sharding import ShardingAxisName
 from tpu_inference.layers.vllm.fused_moe import (FusedMoEBackend,
                                                  fused_moe_apply,
